e24aaedd13803b3663c40ec46043120b0b163802
Matteo Kmeliuskas <matteokmeliuskas@gmail.com>
2025-10-10 13:34:10 -0300

    Remoção dos arquivos da branch release_12.1.2510

--- appserver/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run -d --name totvs_appserver -p 1234:1234 -p 12345:12345 -p 8088:8088 matteokme/totvsprotheus2410:appserver2410
--- licenseserver/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run -d --name totvs_licenseserver -p 5555:5555 -p 2234:2234 -p 8020:8020 matteokme/totvsprotheus2410:licenseserver2410
