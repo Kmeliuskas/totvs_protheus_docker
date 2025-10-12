afc9388773041f341110851acda7d7a20fd90e2b
Matteo Kmeliuskas <matteokmeliuskas@gmail.com>
2025-10-11 22:18:34 -0300

    Ajustes

--- appserver/run.sh
+++ appserver/run.sh
@@ -8,4 +8,4 @@ docker run -d \
     -p 1234:1234 \
     -p 12345:12345 \
     -p 8088:8088 \
-    juliansantosinfo/totvs_appserver:release-2410.build-24.3.0.1.dbapi-24.1.0.0
+    matteokme/totvsprotheus2410:appserver2410
--- dbaccess/run.sh
+++ dbaccess/run.sh
@@ -5,4 +5,4 @@ docker run -d \
     --network totvs \
     -p 7890:7890 \
     -p 7891:7891 \
-    juliansantosinfo/totvs_dbaccess:24.1.0.0
+    matteokme/totvsprotheus2410:dbaccess2410
