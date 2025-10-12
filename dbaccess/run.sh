8a4a50621bf85ae16de37d53340fd1bba7717586
Matteo Kmeliuskas <matteokmeliuskas@gmail.com>
2025-10-11 22:07:08 -0300

    Ajustando a 2410

--- dbaccess/run.sh
+++ dbaccess/run.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
-docker run -d --name totvs_dbaccess --network bridge -p 7890:7890 -p 7891:7891 juliansantosinfo/totvs_dbaccess:24.1.1.0
+docker run -d \
+    --name totvs_dbaccess \
+    --network totvs \
+    -p 7890:7890 \
+    -p 7891:7891 \
+    juliansantosinfo/totvs_dbaccess:24.1.0.0
--- mssql/run.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-docker run -d --name totvs_mssql --network bridge -p 1433:1433 juliansantosinfo/totvs_mssql:release-2510.build-24.3.1.1.dbapi-24.1.1.0
