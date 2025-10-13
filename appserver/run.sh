35b7f75b580e83436da0ff4a3015e0ed1b9a3673
Matteo Kmeliuskas <matteokmeliuskas@gmail.com>
2025-10-13 16:26:33 -0300

    Ajustes do APPSERVER e do DBAccess

--- appserver/run.sh
+++ appserver/run.sh
@@ -1,11 +1,3 @@
 #! /bin/bash
 
-#! /bin/bash
-
-docker run -d \
-    --name totvs_appserver \
-    --network totvs \
-    -p 1234:1234 \
-    -p 12345:12345 \
-    -p 8088:8088 \
-    matteokme/totvsprotheus2410:appserver2410
+docker run -d --name totvs_appserver -p 1234:1234 -p 12345:12345 -p 8088:8088 matteokme/totvsprotheus2410:appserver2410
--- dbaccess/run.sh
+++ dbaccess/run.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
 
-docker run -d \
-    --name totvs_dbaccess \
-    --network totvs \
-    -p 7890:7890 \
-    -p 7891:7891 \
-    matteokme/totvsprotheus2410:dbaccess2410
+docker run -d --name totvs_dbaccess -p 7890:7890 -p 7891:7891 matteokme/totvsprotheus2410:dbaccess2410
